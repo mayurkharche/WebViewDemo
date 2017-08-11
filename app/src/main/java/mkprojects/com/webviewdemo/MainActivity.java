d5f2ebe78de22a8f5c264d7cfe9ef894481ca0b9
Mayur Kharche <mayurkharche1994@gmail.com>
2017-08-11 23:38:51 +0530

    Initial commit

---
+++ app/src/main/java/mkprojects/com/webviewdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package mkprojects.com.webviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private Button btnWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        webView.setWebViewClient(new MyBrowser());
+
+        String url = "https://developers.google.com/cloud-messaging/";
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
